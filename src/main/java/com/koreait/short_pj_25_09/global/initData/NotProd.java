3f427aaa4c04ea2f3386b9b80dba21dbcb5b28da
doomdex84 <doomdex84@gmail.com>
2025-09-25 12:44:05 +0900

    자동배포 세팅-1

--- src/main/java/com/koreait/short_pj_25_09/global/initData/NotProd.java
+++ src/main/java/com/koreait/short_pj_25_09/global/initData/NotProd.java
@@ -11,8 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
-import org.springframework.transaction.annotation.Transactional;
-
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
